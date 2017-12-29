6987906af28fd1de78627250754505d01f3e4020
weetinygit <weetingyit36@gmail.com>
2017-12-30 01:45:15 +0700

    newest

--- tart_arduino_code/tart_arduino_code.ino
+++ tart_arduino_code/tart_arduino_code.ino
@@ -10,9 +10,9 @@
 #define CMD_RECEIVE_ACK 13
 #define BAUD 57600
 
-#define MOTOR_1_RADPERSTEP 360/(1.8*57.2958)
-#define MOTOR_2_RADPERSTEP 360/(1.8*57.2958*(26+103/121))
-#define MOTOR_3_RADPERSTEP 360/(1.8*57.2958*(26+103/121))
+#define MOTOR_1_RADPERSTEP 1.8*0.01745
+#define MOTOR_2_RADPERSTEP 1.8*0.01745
+#define MOTOR_3_RADPERSTEP 1.8*0.01745/(26+103/121)
 
 #include <ros.h>
 #include <std_msgs/String.h>
@@ -29,30 +29,31 @@ int pos2 = 5678;
 int pos3 = 5678;
 double motor[3] = {0,0,0};
 
-int radToSteps(double rad, double radPerStep){
-  return (int)(rad/radPerStep);
+double radToSteps(double rad, double radPerStep){
+  //return rad/radPerStep;
+  return rad*1000;
 }
 
 //Ros communication functions
 void motor1_cmd(const std_msgs::Float32& cmd_msg)
 {
   digitalWrite(CMD_RECEIVE_ACK, HIGH);
-  motor[0] = radiansToDegrees(cmd_msg.data);
-  delay(10);
+  motor[0] = cmd_msg.data;
+  pos1 = (int)radToSteps(motor[0], MOTOR_1_RADPERSTEP);
   digitalWrite(CMD_RECEIVE_ACK, LOW);
 }
 void motor2_cmd(const std_msgs::Float32& cmd_msg)
 {
   digitalWrite(CMD_RECEIVE_ACK, HIGH);
-  motor[1] = radiansToDegrees(cmd_msg.data);
-  delay(10);
+  motor[1] = cmd_msg.data;
+   pos2 = (int)radToSteps(motor[1], MOTOR_2_RADPERSTEP);
   digitalWrite(CMD_RECEIVE_ACK, LOW);
 }
 void motor3_cmd(const std_msgs::Float32& cmd_msg)
 {
   digitalWrite(CMD_RECEIVE_ACK, HIGH);
-  motor[2] = radiansToDegrees(cmd_msg.data);
-  delay(10);
+  motor[2] = cmd_msg.data;
+  pos3 = (int)radToSteps(motor[2], MOTOR_2_RADPERSTEP);
   digitalWrite(CMD_RECEIVE_ACK, LOW);
 }
 
@@ -72,7 +73,8 @@ int status = -1; //starting
 char msg[2] = "1";
 
 void setup()
-{  
+{
+  
   stepper1.setMaxSpeed(3000);
   stepper1.setAcceleration(1000);
   stepper2.setMaxSpeed(2000);
@@ -80,40 +82,45 @@ void setup()
   stepper3.setMaxSpeed(2000);
   stepper3.setAcceleration(800);
   pinMode(TOGGLE_BUTTON, INPUT);
+  pinMode(CMD_RECEIVE_ACK, OUTPUT);
+  digitalWrite(CMD_RECEIVE_ACK, HIGH);
   stepper2.moveTo(pos2);
   stepper3.moveTo(pos3);
   
+  
   //Moves to reference position for motors 2 and 3
-  while(digitalRead(MOTOR_2_LIMIT)||digitalRead(MOTOR_3_LIMIT)){
+  while(digitalRead(MOTOR_2_LIMIT)&&digitalRead(MOTOR_3_LIMIT)){
     stepper2.run();
     stepper3.run();
   }
-  stepper2.stop;
-  if(digitalRead(MOTOR_2_LIMIT)) {
+  
+  digitalWrite(CMD_RECEIVE_ACK, LOW);
+  
+  if(!digitalRead(MOTOR_2_LIMIT)) {
     stepper2.stop();
-    stepper2.setCurrentPosition();
+    stepper2.setCurrentPosition(0);
     while(digitalRead(MOTOR_3_LIMIT)){
       stepper3.run();
     }
     stepper3.stop();
-    stepper3.setCurrentPosition();
+    stepper3.setCurrentPosition(0);
   }
   else {
     stepper3.stop();
-    stepper3.setCurrentPosition();
+    stepper3.setCurrentPosition(0);
     while(digitalRead(MOTOR_2_LIMIT)){
       stepper2.run();
     }
     stepper2.stop();
-    stepper2.setCurrentPosition();
+    stepper2.setCurrentPosition(0);
   }
-
+  
   //Initialize ros commands
   nh.initNode();
   nh.advertise(action_node);
   nh.subscribe(sub);
   nh.subscribe(sub2);
-  pinMode(POSE_SWITCH, INPUT);
+  pinMode(TOGGLE_BUTTON, INPUT);
   pinMode(CMD_RECEIVE_ACK, OUTPUT);
   nh.getHardware()->setBaud(BAUD);
 }
@@ -121,20 +128,18 @@ void setup()
 void loop()
 {
   //Sending toggle command
-  if(digitalRead(POSE_SWITCH) != status){
-    if(digitalRead(POSE_SWITCH) == 1) msg[0] = '2';
-    else if (digitalRead(POSE_SWITCH) == 0) msg[0] = '1';
+  if(digitalRead(TOGGLE_BUTTON) != status){
+    if(digitalRead(TOGGLE_BUTTON) == 1) msg[0] = '2';
+    else if (digitalRead(TOGGLE_BUTTON) == 0) msg[0] = '1';
     str_msg.data = msg;
     action_node.publish( &str_msg );
 
-    status = digitalRead(POSE_SWITCH);
+    status = digitalRead(TOGGLE_BUTTON);
   }
    nh.spinOnce();
 
    //Move motors to position
-   pos1 = radToSteps(motor[0], MOTOR_1_RADPERSTEP);
-   pos2 = radToSteps(motor[1], MOTOR_2_RADPERSTEP);
-   pos3 = radToSteps(motor[2], MOTOR_2_RADPERSTEP);
+
    stepper1.moveTo(pos1);
    stepper2.moveTo(pos2);
    stepper3.moveTo(pos3);
